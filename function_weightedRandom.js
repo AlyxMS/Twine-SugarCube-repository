4edd482c9d5fc4bb92591e183ac2e072073fc267
AlyxMS <alyxms@live.com>
2022-09-28 22:21:05 +0800

    Fixed, again.

--- function_weightedRandom.js
+++ function_weightedRandom.js
@@ -1,7 +1,4 @@
 setup.weightedRandom = function weightedRandom(weightedArray) {
     let randomNumber = weightedArray.reduce((a, b) => [null, a[1] + b[1]])[1] * Math.random();
-    weightedArray.forEach(element => {
-        if (randomNumber <= element[1]) return element[0];
-        randomNumber -= element[1];
-    });
+    for (let element of weightedArray) { if ((randomNumber -= element[1]) < 0) return element[0]; };
 }
