34e20533ef975696449b2b7762060048822df1f8
AlyxMS <alyxms@live.com>
2022-09-28 11:46:11 +0800

    Renamed to numSpread

--- function_numSpread.js
+++ function_numSpread.js
@@ -1,3 +1,3 @@
-setup.numberSpread = function (input, spread, round = 0) {
+setup.numSpread = function numSpread(input, spread, round = 0) {
     return (input + spread * (2 * Math.random() - 1)).toFixed(round);
 }
