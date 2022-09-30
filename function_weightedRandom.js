c285a1267d3cdc71f2edcb40c4a022faca22106a
AlyxMS <alyxms@live.com>
2022-09-30 23:05:22 +0800

    Adopted TME's optimization correctly
    Why TF did I write a 1 instead of 0?
    
    I blame sleep deprivation

--- function_weightedRandom.js
+++ function_weightedRandom.js
@@ -1,4 +1,4 @@
 setup.weightedRandom = function weightedRandom(weightedArray) {
-    let randomNumber = weightedArray.reduce((a, b) => a + b[1], 1) * Math.random();
+    let randomNumber = weightedArray.reduce((a, b) => a + b[1], 0) * Math.random();
     for (let element of weightedArray) { if ((randomNumber -= element[1]) < 0) return element[0]; }
 }
