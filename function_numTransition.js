6ef920f758aa4172b600b165ce6e4d66ee1ce087
AlyxMS <alyxms@live.com>
2022-11-30 13:32:07 +0800

    Save the lines

--- function_numTransition.js
+++ function_numTransition.js
@@ -1,14 +1,12 @@
 setup.numTransition = function numTransition(selector, targetNumber, time = 1000, steps = 20, curve = 0.3, decimal = 0) {
     let target = document.querySelector(selector);
     let originalNumber = parseFloat(target.textContent);
-    let interval = time / steps;
-    let difference = targetNumber - originalNumber;
     for (let i = 0; i < steps; i++) {
         setTimeout(function () {
             target.textContent = rounder(
-                originalNumber + difference * Math.pow((i + 1) / steps, curve)
+                originalNumber + (targetNumber - originalNumber) * Math.pow((i + 1) / steps, curve)
             )
-        }, interval * i);
+        }, time / steps * i);
     }
     function rounder(inputNum) {
         if (decimal) return inputNum.toFixed(decimal);
